7bf0aa0e235c5ee2e185a5d892e19063f14388f7
hannah <130411096+HannahRoach@users.noreply.github.com>
2023-04-14 15:30:56 -0500

    Add files via upload
    line one adds variables to the list.
    line three, four and five gives the variables a place in the list.
    lines seven, eight and nine prints the variables in the list.

---
+++ mylist.py
@@ -0,0 +1,9 @@
+berries = ["strawberry", "blueberry", "blackberry"]
+
+first_berry = berries[0]
+second_berry = berries[1]
+third_berry = berries[2]
+
+print("the first berry on the berries list is %s" % first_berry)
+print("the second berry on the berries list is %s" % second_berry)
+print("the thirst berry on the berries list is %s" % third_berry)
