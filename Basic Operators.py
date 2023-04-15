5439e897e9a112ba9560d51f980550e4719b1797
hannah <130411096+HannahRoach@users.noreply.github.com>
2023-04-15 10:54:31 -0500

    Add files via upload
    lines one set up a math problem.
    line two print the answer.
    line four sets up a remainder math problem.
    line five print the correct answer.
    line seven and eight is setting a squared and a cubed math problem.
    line nine and ten prints the correct answers.
    line 12 is setting a variables and multiplying hello by ten.
    line 13 print ten hellos.

---
+++ Basic Operators.py
@@ -0,0 +1,13 @@
+number = 5 + 6 * 3 / 4.0
+print(number)
+
+remainder = 11 % 3
+print(remainder)
+
+squared = 7 ** 2
+cubed = 2 ** 3
+print(squared)
+print(cubed)
+
+lotsofhellos = "hello" * 10
+print(lotsofhellos)
